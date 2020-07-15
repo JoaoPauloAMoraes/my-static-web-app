30b249a38a15beb0c446ee10958da652fbb1ae2e
JoaoPaulo <jpm@genteterra.com.br>
2020-07-15 18:41:45 -0300

    api changes

--- api/products-post/index.js
+++ api/products-post/index.js
@@ -1,15 +1,9 @@
 const data = require('../shared/product-data');
 
 module.exports = async function (context, req) {
-  const product = {
-    id: undefined,
-    name: req.body.name,
-    description: req.body.description,
-  };
-
   try {
-    const newProduct = data.addProduct(product);
-    context.res.status(201).json(newProduct);
+    const products = data.getProducts();
+    context.res.status(200).json(products);
   } catch (error) {
     context.res.status(500).send(error);
   }
